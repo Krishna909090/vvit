746bb1017c960cc9515f35d76fbf6f7e2e6b06cc
Krishna909090 <bhsrikrishna1994@gmail.com>
2026-02-01 10:24:14 +0530

    change in invoice

--- src/utils/invoiceGenerator.ts
+++ src/utils/invoiceGenerator.ts
@@ -45,10 +45,6 @@ export const generateInvoicePDF = async (
       doc.on('data', buffers.push.bind(buffers))
       doc.on('end', () => resolve(Buffer.concat(buffers)))
       doc.on('error', reject)
-
-      // Top Half - Student Copy
-      drawInvoiceInstance(doc, data, 0, 'STUDENT COPY')
-
       // Cut Line (Dashed)
       const midY = 421;
       doc
@@ -59,9 +55,6 @@ export const generateInvoicePDF = async (
          .stroke();
       
       doc.undash(); // Reset dash
-      
-      // Bottom Half - Office Copy
-      drawInvoiceInstance(doc, data, 421, 'OFFICE COPY')
 
       doc.end()
     } catch (err) {
